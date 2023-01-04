4b47dbc28d157adc4b8933441d2ebe15464ba7d9
AbdAlmjed Skaik <ab0d-33@hotmail.com>
2023-01-04 11:53:38 +0200

    .

--- src/webparts/salic/components/App/Global/AntdLoader/AntdLoader.js
+++ src/webparts/salic/components/App/Global/AntdLoader/AntdLoader.js
@@ -6,7 +6,7 @@ import { Spin, Typography } from 'antd';
 const AntdLoader = ({ customStyle, label }) => {
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '100px 25px 25px 25px', width: '100%', ...customStyle}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '100px 0 25px 0', width: '100%', ...customStyle}}>
       <Spin indicator={<LoadingOutlined spin />} />
       <Typography.Text style={{color: 'var(--link-text-color)', fontSize: '.8rem'}}>{label || "Loading..."}</Typography.Text>
     </div>
